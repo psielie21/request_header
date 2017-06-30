92df7d9ca49ea1dfc0750993651d71166a57bf87
Carlo Kneissl <psielisorigin@gmail.com>
2017-06-30 06:13:29 +0000

    Finished working on the API project

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require("express")
+var get_ip = require("ipware")().get_ip
+var useragent = require("useragent")
+
+var app = express()
+
+app.get("/", function(req, res){
+    console.log(req.headers);
+    var agent = useragent.parse(req.headers['user-agent']);
+    var obj = {"ipadress": get_ip(req)["clientIp"] , "language": req.headers["accept-language"].substring(0,5), "software": agent.os.toString()};
+    res.end(JSON.stringify(obj));
+})
+
+app.listen(8080)
